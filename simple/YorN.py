544da28ea47375885bcf86afebb2140db37b1f6c
AnamariaL <32013978+AnamariaL@users.noreply.github.com>
2021-06-21 10:59:56 +0300

    Add files via upload

---
+++ simple/YorN.py
@@ -0,0 +1,14 @@
+#from random import choice
+import random
+
+for i in range(1):
+    answer = random.randint(1,2)
+    if answer  % 2 == 0:
+        print("YES!!!")
+
+    else:
+        print("NO!!")
+
+        
+#answer = choice(['yes','no'])
+#print(answer)        
